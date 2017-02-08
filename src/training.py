ff1cac0bccb57b1e105544692b68836846c3d947
NilsG-S <NilsG-S@users.noreply.github.com>
2017-02-08 15:54:20 -0600

    Implemented data cleaning

--- src/training.py
+++ src/training.py
@@ -2,14 +2,33 @@ import csv as csv
 import pandas as pandas
 
 
-def data_handler(pas, sur, ages, fares, width, row):
+def age_data(ages, row):
+    # Append current row value to correct entry in ages
+    ages[(row['Sex'], row['Pclass'])].append(row['Age'])
+
+
+def data_cleaning(age_medians, row):
+    if row['Sex'] == 'female':
+        row['Gender'] = 0
+    else:
+        row['Gender'] = 1
+
+    if pandas.isnull(row['Age']):
+        row['AgeFill'] = age_medians[(row['Sex'], row['Pclass'])]
+        row['AgeIsNull'] = 1
+    else:
+        row['AgeFill'] = row['Age']
+        row['AgeIsNull'] = 0
+
+    return row
+
+
+def data_gathering(pas, sur, fares, width, row):
     # Performs a floor operation at intervals of bin_width
     # If the result cannot be found, return fare bracket 4
     fare = fares.get(row[9] // width, 4)
     # Add 1 to the given category in the passengers dict
     pas[(row[4], row[2], fare)] += 1
-    # Append current row value to correct entry in ages
-    ages[(row[4], row[2], fare)].append(row[5])
     if int(row[1]) == 1:
         # If the passenger survived add 1 to the correct category
         sur[(row[4], row[2], fare)] += 1
@@ -20,17 +39,32 @@ def training():
     model_file = open('../model/model.csv', 'w', newline='')
     model_object = csv.writer(model_file)
     # Open the training data set
-    train_file = open('../data/train.csv', newline='')
-    train_object = csv.reader(train_file)
-    # Skip the header row
-    next(train_object)
-
     data = pandas.read_csv('../data/train.csv', header=0)
+
+    # Data cleaning
+
+    ages = {}
+    age_medians = {}
+
+    for sex in ("female", "male"):
+        for p_class in range(1, 4):
+            ages[(sex, p_class)] = []
+            age_medians[(sex, p_class)] = 0
+
+    data.apply(lambda x: age_data(ages, x), axis=1)
+
+    for sex in ("female", "male"):
+        for p_class in range(1, 4):
+            age_medians[(sex, p_class)] = \
+                pandas.Series(ages[(sex, p_class)]).median()
+
+    data = data.apply(lambda x: data_cleaning(age_medians, x), axis=1)
+
+    # Data gathering
 
     # Dictionaries with keys based on sex, passenger class, and fare bracket
     passengers = {}
     survived = {}
-    ages = {}
 
     # Initializing the dictionaries to 0
     for sex in ("female", "male"):
@@ -38,7 +72,6 @@ def training():
             for fare in range(1, 5):
                 passengers[(sex, p_class, fare)] = 0
                 survived[(sex, p_class, fare)] = 0
-                ages[(sex, p_class, fare)] = []
 
     # A dictionary to match an integer value to a fare bracket
     # Fair bracket 1 is for fare =< 10
@@ -55,11 +88,10 @@ def training():
     # Width of the fare brackets
     bin_width = 10
 
-    data.apply(lambda x: data_handler(passengers, survived, ages, fare_group,
-                                      bin_width, x), axis=1)
+    data.apply(lambda x: data_gathering(passengers, survived, fare_group,
+                                        bin_width, x), axis=1)
 
-    # data['Gender'] = data['Sex'].map({'female': 0, 'male': 1}).astype(int)
-    # median_ages[i, j] = df[(df['Gender'] == i) & (df['Pclass'] == j + 1)]['Age'].dropna().median()
+    # Data analysis
 
     model_object.writerow(["Sex", "Pclass", "Fare", "Prediction"])
     for sex in ("female", "male"):
@@ -84,6 +116,5 @@ def training():
                 model_object.writerow([sex, p_class, fare, prediction])
 
     model_file.close()
-    train_file.close()
 
 training()
