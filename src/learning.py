4fb82989188d77a763ce966e1ef551bb7ab68737
NilsG-S <NilsG-S@users.noreply.github.com>
2017-02-16 15:54:37 -0600

    Decision tree is functional

--- src/learning.py
+++ src/learning.py
@@ -1,12 +1,41 @@
 import pandas as pandas
-import numpy as numpy
+from sklearn import tree
 
 
 def learn():
-    engineer_data = pandas.read_csv('../engineered/engineer_train.csv', header=0)
+    engineer_train = pandas.read_csv(
+        '../engineered/engineer_train.csv', header=0
+    )
+    engineer_test = pandas.read_csv(
+        '../engineered/engineer_test.csv', header=0
+    )
 
-    train_data = engineer_data.values
+    train_features = engineer_train[
+        ["AgeFill",
+         "Fare",
+         "Gender",
+         "Pclass",
+         "FamilySize"]
+    ].values
 
-    print(train_data[0::, 5])
+    engineer_test = engineer_test[engineer_test["Fare"].map(
+        lambda x: False if pandas.isnull(x) else True
+    )]
+
+    test_features = engineer_test[
+        ["AgeFill",
+         "Fare",
+         "Gender",
+         "Pclass",
+         "FamilySize"]
+    ].values
+
+    target = engineer_train["Survived"].values
+
+    train_tree = tree.DecisionTreeClassifier()
+    train_tree.fit(train_features, target)
+
+    print(train_tree.feature_importances_)
+    print(train_tree.predict(test_features))
 
 learn()
