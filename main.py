989e85e3988c6989ded1a2104563d2830b43a754
NilsG-S <NilsG-S@users.noreply.github.com>
2017-03-10 04:39:35 -0600

    Really fixed import problems

---
+++ main.py
@@ -0,0 +1,64 @@
+import src.cleaning as cleaning
+import src.modeling as modeling
+import src.testing as testing
+import src.engineering as engineering
+import src.learning as learning
+
+
+# Old
+
+def call_gen_model():
+    modeling.gen_model()
+
+
+def call_test():
+    testing.test()
+
+
+# New
+
+def call_clean():
+    cleaning.clean('data/train.csv', 'cleaned/clean_train.csv')
+    cleaning.clean('data/test.csv', 'cleaned/clean_test.csv')
+
+    return True
+
+
+def call_gen_features():
+    engineering.gen_features(
+        'cleaned/clean_train.csv',
+        'engineered/engineer_train.csv'
+    )
+    engineering.gen_features(
+        'cleaned/clean_test.csv',
+        'engineered/engineer_test.csv'
+    )
+
+    return True
+
+
+def call_learn():
+    learning.learn()
+
+    return True
+
+
+def call_exit():
+    return False
+
+
+def main():
+    control = True
+    switch = {
+        "clean": call_clean,
+        "gen_features": call_gen_features,
+        "learn": call_learn,
+        "exit": call_exit
+    }
+
+    while control:
+        command = input("Enter a command: ")
+
+        control = switch[command]()
+
+main()
--- src/learning.py
+++ src/learning.py
@@ -4,10 +4,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 def learn():
     engineer_train = pandas.read_csv(
-        '../engineered/engineer_train.csv', header=0
+        'engineered/engineer_train.csv', header=0
     )
     engineer_test = pandas.read_csv(
-        '../engineered/engineer_test.csv', header=0
+        'engineered/engineer_test.csv', header=0
     )
 
     train_features = engineer_train[
@@ -40,4 +40,4 @@ def learn():
     for index in range(0, len(passengers)):
         data.append([passengers[index], predictions[index]])
 
-    pandas.DataFrame(data).to_csv('../output/random_forest.csv', index=False, header=0)
+    pandas.DataFrame(data).to_csv('output/random_forest.csv', index=False, header=0)
